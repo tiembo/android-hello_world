19867c314c4913c0a179a59292f61fadf7dd0d3c
Tiem Song <tiem@songfamily.org>
2012-03-25 23:11:34 -0700

    add text, update colors, woo

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -10,6 +10,9 @@ package com.example;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int string_color=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
@@ -17,6 +20,7 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int explanation=0x7f050001;
     }
 }
