a5591d2ab734004726adc7686a791def51eae871
unknown <bevoclea@ndl.(none)>
2012-03-27 22:31:43 -0700

    grid view

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -15,6 +15,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int sample_0=0x7f020001;
+        public static final int sample_1=0x7f020002;
+        public static final int sample_2=0x7f020003;
+        public static final int sample_3=0x7f020004;
+        public static final int sample_4=0x7f020005;
+        public static final int sample_5=0x7f020006;
+        public static final int sample_6=0x7f020007;
+        public static final int sample_7=0x7f020008;
+    }
+    public static final class id {
+        public static final int gridview=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,6 +2,10 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity
 {
@@ -11,5 +15,14 @@ public class MyActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MyActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
